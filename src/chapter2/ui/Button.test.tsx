495ab3b92f68d3330378dba9890d6e65f54434db
kondo takeshi <chaspy@users.noreply.github.com>
2023-03-25 19:24:59 +0900

    snap

---
+++ src/chapter2/ui/Button.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import { Button } from "./Button";
+
+describe("Button", () => {
+  // react-test-rendererを利用した、スナップショットテスト
+  it("renders correctly with react-test-renderer", () => {
+    const button = renderer.create(<Button />);
+    expect(button.toJSON()).toMatchSnapshot();
+  });
+});
---
+++ src/chapter2/ui/Button.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useState } from "react";
+export const Button = () => {
+  const [isToggleOn, setIsToggleOn] = useState(true);
+  return (
+    <button onClick={() => setIsToggleOn(!isToggleOn)}>
+      {isToggleOn ? "ON" : "OFF"}{" "}
+    </button>
+  );
+};
